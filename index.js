d5fb02f836934193a23b1ca746fdcdd55b99aac9
d3 <d3@gmb.com>
2019-10-21 10:37:53 +0100

    Add closures for containment

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 const specer = require('./lib/morxspecer');
-const extractor = require('./lib/paramextractor');
+const extractorFunc = require('./lib/paramextractor');
 
-const morx = {};
+/**
+ * If you call morxExport()
+ * You get a closure that allows you
+ * have contained config for each morx instance you require
+ * 
+ * For backward compatibility, we will get default values and set on the
+ * export function as well so morx works as it used to
+ */
 
-morx.spec = specer.spec;
-morx.validate = extractor.processParams;
-morx.registerValidator = extractor.registerValidator;
-morx.registerFilter = extractor.registerFilter;
-morx.setCustomErrorClass = extractor.setCustomErrorClass;
-module.exports = morx;
+const globalExtractor = extractorFunc();
+function morxExport () {
+  const morx = {};
+  const extractor = extractorFunc();
+  morx.spec = specer.spec;
+  morx.validate = extractor.processParams;
+  morx.registerValidator = extractor.registerValidator;
+  morx.registerFilter = extractor.registerFilter;
+  morx.setCustomErrorClass = extractor.setCustomErrorClass;
+  morx.getCustom = extractor.getCustom; 
+  return morx;
+}
+
+morxExport.spec = specer.spec;
+morxExport.validate = globalExtractor.processParams;
+morxExport.registerValidator = globalExtractor.registerValidator;
+morxExport.registerFilter = globalExtractor.registerFilter;
+morxExport.setCustomErrorClass = globalExtractor.setCustomErrorClass;
+morxExport.getCustom = globalExtractor.getCustom;
+module.exports = morxExport;
